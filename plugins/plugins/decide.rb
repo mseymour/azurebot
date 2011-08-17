47d458de1ad9771439bb3d1647fd3726fcc2bb9d
Mark Seymour <mark.seymour.ns@gmail.com>
2011-08-17 19:45:19 -0300

    Merging new bot code into master

---
+++ plugins/plugins/decide.rb
@@ -0,0 +1,29 @@
+# -*- coding: UTF-8 -*-
+
+require_relative '../ext/string'
+
+class Decide
+	include Cinch::Plugin
+	set(
+		plugin_name: "Decider",
+		help: "Helps you decide on things\nUsage: !decide [a list of items separated by \", \", \", or\", or \" or \".\nUsage: !coin]")
+
+	def decide!(list)
+		list = list.irc_strip_colors
+		options = list.gsub(/ or /i, ",").split(",").map(&:strip).reject(&:empty?)
+		options[Random.new.rand(1..options.length)-1]
+	end
+
+	match %r{decide (.+)}, method: :execute_decision
+	match %r{choose (.+)}, method: :execute_decision
+	def execute_decision(m, list)
+		m.safe_reply("I choose \"#{decide! list}\"!",true);
+	end
+	
+	match %r{coin$}, method: :execute_coinflip
+	def execute_coinflip(m)
+		face = Random.new.rand(1..2) == 1 ? "heads" : "tails";
+		m.safe_reply("I choose \"#{face}\"!",true);
+	end
+
+end
