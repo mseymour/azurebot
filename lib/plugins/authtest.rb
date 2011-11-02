feaf7419e723ce274743f4484cdb149bd91e7950
Mark Seymour <mark.seymour@live.ca>
2011-11-02 03:02:38 -0200

    Fixing tabbing.

--- lib/plugins/authtest.rb
+++ lib/plugins/authtest.rb
@@ -1,22 +1,22 @@
 module Plugins
-	class Authtest
-		include Cinch::Plugin
-		set(
-			plugin_name: "authtest",
-			help: "Tells you if you are logged into the bot or not. (Admins only)",
-			prefix: /^/,
-			suffix: /\?$/,
-			required_options: [:admins])
-			
-		match /Am I (logged in|an admin)/i
-		def execute m, question_context
-			test = config[:admins].logged_in? m.user.mask
-			if test
-				m.channel.action "hugs #{m.user.nick}" if m.channel?
-				m.reply "You are #{question_context}!"
-			else
-				m.reply "Sorry to say this #{m.user.nick}, but you are not #{question_context}."
-			end
-		end
-	end
+  class Authtest
+    include Cinch::Plugin
+    set(
+      plugin_name: "authtest",
+      help: "Tells you if you are logged into the bot or not. (Admins only)",
+      prefix: /^/,
+      suffix: /\?$/,
+      required_options: [:admins])
+      
+    match /Am I (logged in|an admin)/i
+    def execute m, question_context
+      test = config[:admins].logged_in? m.user.mask
+      if test
+        m.channel.action "hugs #{m.user.nick}" if m.channel?
+        m.reply "You are #{question_context}!"
+      else
+        m.reply "Sorry to say this #{m.user.nick}, but you are not #{question_context}."
+      end
+    end
+  end
 end
