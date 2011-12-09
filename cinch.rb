7bdec6501b9d35b192003087c932d1e9a54cf772
Mark Seymour <mark.seymour.ns@gmail.com>
2011-12-09 13:38:14 -0400

    Updating the sample bot config file since it is fairly out of date.

--- cinch.rb
+++ cinch.rb
@@ -9,6 +9,10 @@ require 'modules/admin'
 require 'cinch/plugins/basic_ctcp'
 require 'cinch/plugins/downforeveryone'
 require 'cinch/plugins/identify'
+require 'cinch/plugins/joker'
+require 'cinch/plugins/last_seen'
+require 'cinch_hangman'
+require 'cinch/plugins/fortune'
 require 'plugins/8ball'
 require 'plugins/adminhandler'
 require 'plugins/antispam' 
@@ -25,9 +29,10 @@ require 'plugins/jargonfile'
 require 'plugins/joinnotice'
 require 'plugins/kickban'
 require 'plugins/multiqdb'
+require 'plugins/ping'
 require 'plugins/privtoolbox'
-require 'plugins/questionperiod'
 require 'plugins/rainbow'
+require 'plugins/remoteadmin'
 require 'plugins/russianroulette'
 require 'plugins/ryder'
 require 'plugins/silly'
@@ -51,8 +56,13 @@ bot = Cinch::Bot.new do
       Cinch::Plugins::BasicCTCP,
       Cinch::Plugins::DownForEveryone,
       Cinch::Plugins::Identify,
+      Cinch::Plugins::Joker,
+      Cinch::Plugins::LastSeen,
+      Cinch::Plugins::Hangman,
+      Cinch::Plugins::Fortune,
       Plugins::AdminHandler,
-      Plugins::AntiSpam,
+      Plugins::AntiSpam::Kicker,
+      Plugins::AntiSpam::Lister,
       Plugins::Authtest,
       Plugins::Eightball,
       Plugins::Attack,
@@ -67,9 +77,10 @@ bot = Cinch::Bot.new do
       Plugins::JoinNotice,
       Plugins::Kickban,
       Plugins::MultiQDB,
+      Plugins::Ping,
       Plugins::PrivToolbox,
-      Plugins::QuestionPeriod,
       Plugins::Rainbow,
+      Plugins::RemoteAdmin,
       Plugins::RussianRoulette,
       Plugins::Ryder,
       Plugins::Silly,
@@ -79,20 +90,19 @@ bot = Cinch::Bot.new do
       Plugins::Weather ]
     
     admins = Admin.instance
-    admins.password = File.open('G:/bot/config/admin-password', &:gets)
+    admins.password = File.open(File.dirname(__FILE__) + '/config/admin-password', &:gets)
 
     common_config = {admins: admins}
 
-    c.plugins.options[Plugins::Authtest] = c.plugins.options[Plugins::AdminHandler] = c.plugins.options[Plugins::Toolbox] = c.plugins.options[Plugins::PrivToolbox] = common_config
+    c.plugins.options[Plugins::Authtest] = c.plugins.options[Plugins::AdminHandler] = c.plugins.options[Plugins::Toolbox] = c.plugins.options[Plugins::PrivToolbox] = c.plugins.options[Plugins::RemoteAdmin] = c.plugins.options[Plugins::AntiSpam::Lister] = common_config
 
-    #c.plugins.options[Cinch::Plugins::Identify] = { password: "5CL47K", type: :nickserv }
-    c.plugins.options[Plugins::AntiSpam] = { limit_commands: 5, limit_seconds: 30 }
-    c.plugins.options[Plugins::Attack] = { attack_dictionary: 'G:/bot/config/attackdict.yaml' }
+    c.plugins.options[Plugins::AntiSpam::Kicker] = { limit_commands: 5, limit_seconds: 60 }
+    c.plugins.options[Plugins::Attack] = { attack_dictionary: File.dirname(__FILE__) + '/config/attackdict.yaml' }
     c.plugins.options[Plugins::AutoOP] = { enabled_channels: [] }.merge common_config
     c.plugins.options[Plugins::AutoVoice] = { enabled_channels: [] }.merge common_config
-    c.plugins.options[Plugins::BotInfo] = { owner: "Azure", bot: c.nick, template: 'G:/bot/config/info_template.txt'}.merge common_config
-    c.plugins.options[Plugins::JoinNotice] = { greetings: 'G:/bot/config/greetings/freenode/', filext: '.txt' }
-    c.plugins.options[Plugins::Twitter5] = { access_keys: 'G:/bot/config/twitter_oauth.yaml' }
+    c.plugins.options[Plugins::BotInfo] = { owner: "Azure", bot: c.nick, template: File.dirname(__FILE__) + '/config/info_template.txt'}.merge common_config
+    c.plugins.options[Plugins::JoinNotice] = { greetings: File.dirname(__FILE__) + '/config/greetings/freenode/', filext: '.txt' }
+    c.plugins.options[Plugins::Twitter5] = { access_keys: File.dirname(__FILE__) + '/config/twitter_oauth.yaml' }
   end
 
 end
