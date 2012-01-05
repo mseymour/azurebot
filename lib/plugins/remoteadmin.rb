4840a6bd844c6e9134036a20985b2611385c09a9
Mark Seymour <mark.seymour.ns@gmail.com>
2012-01-05 16:34:25 -0400

    Hacky-hack-hack. At least there's no more repeated admin messages.

--- lib/plugins/remoteadmin.rb
+++ lib/plugins/remoteadmin.rb
@@ -6,22 +6,44 @@ module Plugins
 
     set plugin_name: "Remote admin", help: "Relays certain messages to logged-in admins.", required_options: [:admins]
 
-    listen_to :notice
-    listen_to :private
-    listen_to :ctcp
-    def listen m, message = nil, target = nil
-      return if m.ctcp? && m.ctcp_message.split[0].casecmp("action") == 0
+    listen_to :private, method: :listen_private
+    def listen_private m, message = nil, target = nil
+      return if m.command == "NOTICE" || m.ctcp?
       config[:admins].each_admin {|nick, username, host|
         admin = User(nick)
         next if m.user.nick.casecmp(admin.nick) == 0
-        string = if m.ctcp?
-          m.ctcp_message
-        elsif !message.nil?
+        string = if !message.nil?
           message
         else
           m.message
         end
-        admin.msg fmt_message(nick: m.user.nick, type: (m.ctcp? ? "CTCP" : m.command), string: string)
+        admin.msg fmt_message(nick: m.user.nick, type: "PRIVMSG", string: string)
+      }
+    end
+
+    listen_to :notice, method: :listen_notice
+    def listen_notice m, message = nil, target = nil
+      return if m.command == "PRIVMSG" && m.ctcp?
+      config[:admins].each_admin {|nick, username, host|
+        admin = User(nick)
+        next if m.user.nick.casecmp(admin.nick) == 0
+        string = if !message.nil?
+          message
+        else
+          m.message
+        end
+        admin.msg fmt_message(nick: m.user.nick, type: "NOTICE", string: string)
+      }
+    end
+
+    listen_to :ctcp, method: :listen_ctcp
+    def listen_ctcp m, message = nil, target = nil
+      return if m.command == "NOTICE" || m.ctcp_message.split[0].casecmp("action") == 0
+      config[:admins].each_admin {|nick, username, host|
+        admin = User(nick)
+        #next if m.user.nick.casecmp(admin.nick) == 0
+        string = m.ctcp_message
+        admin.msg fmt_message(nick: m.user.nick, type: "CTCP", string: string)
       }
     end
 
@@ -52,7 +74,7 @@ module Plugins
         type: "---",
         string: ""
       }.merge params
-      "![b]%<type>s![b] [%<nick>s] %<string>s".irc_colorize % params
+      "#{Format(:bold, "%<type>s")} [%<nick>s] %<string>s" % params
     end
 
   end
