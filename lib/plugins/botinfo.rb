931abaec760001ef195c46b2fc0602222a1a7700
Mark Seymour <mark.seymour.ns@gmail.com>
2012-01-21 19:16:05 -0400

    Updating BotInfo to the latest revision + new format.

--- lib/plugins/botinfo.rb
+++ lib/plugins/botinfo.rb
@@ -1,5 +1,5 @@
 # -*- coding: UTF-8 -*-
-
+require 'tag_formatter'
 require 'active_support/core_ext/object/blank'
 require 'active_support/core_ext/array/conversions'
 require_relative '../obj_ext/string'
@@ -12,51 +12,9 @@ module Plugins
 
     set(
       plugin_name: "Botinfo",
-      help: "Notices you information about me.\nUsage: `![botnick]`",
-      required_options: [:template, :owner, :bot, :admins])
-
-      def regexp_process (local_r, bot_r = nil)
-        if !local_r.blank?
-          local_r.is_a?(Regexp) ? local_r.source : local_r.to_s
-        elsif !bot_r.blank?
-          bot_r.is_a?(Regexp) ? bot_r.source : bot_r.to_s
-        else
-          ""
-        end
-      end
-
-      match /generate documentation/, method: :e_rpc # Yes, I know that the command is a misnomer. -e: Not now?
-      def e_rpc m
-        return unless config[:admins].logged_in?(m.user.mask)
-
-        documentation = []
-        documentation << "Azurebot"
-        documentation << "========"
-        documentation << ""
-        documentation << "A Ruby-powered IRC bot using the [Cinch IRC bot framework](https://github.com/cinchrb/cinch \"Cinch at Github\")"
-        documentation << ""
-        
-        documentation << "Plugins"
-        documentation << "-------"
-        documentation << ""
-        @bot.plugins.each {|p| 
-          next if p.class.help.blank?
-          documentation << "### #{p.class.plugin_name} (`#{p.class.name}`)"
-          documentation << ""
-          documentation << "#{p.class.help.gsub("\n","\n\n")}"
-          documentation << ""
-        };
-
-        documentation << "Author information"
-        documentation << "------------------"
-        documentation << "* Mark Seymour ('Azure')"
-        documentation << "* Email: <mark.seymour.ns@gmail.com>"
-        documentation << "* WWW: <http://lain.rustedlogic.net/>"
-        documentation << "* IRC: #shakesoda @ irc.freenode.net"
-
-        File.open("#{File.expand_path("~")}/azurebot_documentation.md", 'w') {|f| f.write(documentation.join("\n")) }
-      end
-
+      help: "Notices you information about me.\nUsage: `/msg <nick> info`\nUsage: `/msg <nick> list plugins`",
+      required_options: [:template, :owner, :bot, :admins],
+      react_on: :private)
 
     # How to config:
     # :admin -- admin class instance
@@ -65,37 +23,30 @@ module Plugins
     # :bot -- What to display for the "bot_name" field.
     # All fields in the text file must be surrounded by '<>', and lines can be commented out using '#'.
 
-    def format_notice! user
-      plugin_list = [];
-      @bot.plugins.each {|p| 
-        plugin_list << p.class.plugin_name
-      };
-
-      template = open(config[:template], &:read).gsub(/<(\w+)>/, "%<#{'\1'.downcase}>s").split("\n").delete_if {|d| d =~ /^#.+/}.reject(&:blank?).join("\n");
-
-      template % {
-        bot_name: config[:bot],
-        owner_name: config[:admins].first_nick || config[:owner],
+    match /^info$/i, use_prefix: false
+    def execute(m)
+      tags = {
+        bot_name: @bot.nick,
         cinch_version: Cinch::VERSION,
         is_admin: config[:admins].is_admin?(user.mask) ? "an admin" : "not an admin",
-        uptime: time_diff_in_natural_language(@bot.signed_on_at, Time.now, acro: true),
-        session_start_date: @bot.signed_on_at.strftime("%A, %B %e, %Y, at %l:%M:%S %P"),
-        plugins: plugin_list.to_sentence,
-        plugins_head: plugin_list[0..9].join(", "),
-        plugin_count_remaining: plugin_list.length - 10,
-        ruby_version: RUBY_VERSION,
+        owner_name: config[:admins].first_nick || config[:owner],
+        plugins_count_remaining: @bot.plugins.length - 9,
+        plugins_head: @bot.plugins[0..9].each {|p| p.class.plugin_name }.join(", "),
         ruby_platform: RUBY_PLATFORM,
-        ruby_release_date: RUBY_RELEASE_DATE
+        ruby_release_date: RUBY_RELEASE_DATE,
+        ruby_version: RUBY_VERSION,
+        session_start_date: @bot.signed_on_at.strftime("%A, %B %e, %Y, at %l:%M:%S %P"),
+        total_channels: "(SOON)",
+        total_users: "(SOON)",
+        uptime: time_diff_in_natural_language(@bot.signed_on_at, Time.now, acro: true)
       }
+      
+      tf = TagFormatter.new config[:template_path], tags: tags
+
+      m.user.notice tf.parse!
     end
 
-    match /(.+)$/i
-    def execute(m, nick)
-      return unless (nick.irc_downcase(:rfc1459) <=> config[:bot].irc_downcase(:rfc1459)) == 0
-      m.user.notice(format_notice!(m.user).irc_colorize)
-    end
-
-    match /plugins$/i, method: :execute_list
+    match /list plugins$/i, method: :execute_list, use_prefix: false
     def execute_list m
       list = []
       @bot.plugins.each {|p| list << p.class.plugin_name };
