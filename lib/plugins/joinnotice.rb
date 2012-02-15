e2791a331f108350c337bf85282cdc769e7fbb49
Mark Seymour <mark.seymour.ns@gmail.com>
2012-02-15 19:59:23 -0400

    Make the notices not show the line number, and instead just simply show the channel name.

--- lib/plugins/joinnotice.rb
+++ lib/plugins/joinnotice.rb
@@ -11,19 +11,14 @@ module Plugins
     def get_channel_greeting channel
       open([config[:greetings], channel, config[:filext]].join, &:read) rescue nil
     end
-    
+
     listen_to :join
     match /hello$/, method: :listen
-    def listen(m)
+    def listen m
       return if m.user.nick == @bot.nick
       greeting = get_channel_greeting(m.channel.name)
-      return if greeting.nil?
-      notice = greeting.split("\n")
-      notice.delete_if{|e| e.match(/^\/\//)} # Line is commented out.
-      m.user.notice(notice.each_with_index.map {|element, index| 
-        index_s = " #{index+1}/#{notice.length}" if notice.length >= 2
-        "[#{m.channel.name}#{index_s}] #{element}"
-      }.join("\n")) unless notice.blank?
+      return if greeting.blank?
+      m.user.notice greeting.split($/).delete_if{|e| e.match(/^\/\//)}.map {|e| "[#{m.channel.name}] #{e.to_s}"}.join($/)
     end
 
   end
