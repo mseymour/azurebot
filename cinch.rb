a85f17c38f83752082b2021f7a3f2fad613df121
Mark Seymour <mark.seymour.ns@gmail.com>
2012-03-03 16:04:15 -0400

    Updating sample bootstrap script to reflect my own bot on Freenode.

--- cinch.rb
+++ cinch.rb
@@ -1,4 +1,6 @@
+#gem 'cinch', '>= 2.0.0'
 require 'cinch'
+require 'cinch/logger/zcbot_logger'
 
 class Cinch::User; def hash; super; end; end;
 
@@ -9,10 +11,6 @@ require 'modules/admin'
 require 'cinch/plugins/basic_ctcp'
 require 'cinch/plugins/downforeveryone'
 require 'cinch/plugins/identify'
-require 'cinch/plugins/joker'
-require 'cinch/plugins/last_seen'
-require 'cinch_hangman'
-require 'cinch/plugins/fortune'
 require 'plugins/8ball'
 require 'plugins/adminhandler'
 require 'plugins/antispam' 
@@ -22,12 +20,15 @@ require 'plugins/autoop'
 require 'plugins/autovoice'
 require 'plugins/booru'
 require 'plugins/botinfo'
+require 'plugins/byedaddy'
 require 'plugins/decide'
 require 'plugins/delayedrejoin'
 require 'plugins/dice'
+require 'plugins/djinfo'
 require 'plugins/jargonfile'
 require 'plugins/joinnotice'
 require 'plugins/kickban'
+require 'plugins/macros'
 require 'plugins/multiqdb'
 require 'plugins/ping'
 require 'plugins/privtoolbox'
@@ -36,6 +37,7 @@ require 'plugins/remoteadmin'
 require 'plugins/russianroulette'
 require 'plugins/ryder'
 require 'plugins/silly'
+require 'plugins/time_ban'
 require 'plugins/toolbox'
 require 'plugins/twitter'
 require 'plugins/urbandictionary'
@@ -56,10 +58,6 @@ bot = Cinch::Bot.new do
       Cinch::Plugins::BasicCTCP,
       Cinch::Plugins::DownForEveryone,
       Cinch::Plugins::Identify,
-      Cinch::Plugins::Joker,
-      Cinch::Plugins::LastSeen,
-      Cinch::Plugins::Hangman,
-      Cinch::Plugins::Fortune,
       Plugins::AdminHandler,
       Plugins::AntiSpam::Kicker,
       Plugins::AntiSpam::Lister,
@@ -70,12 +68,15 @@ bot = Cinch::Bot.new do
       Plugins::AutoVoice,
       Plugins::Booru,
       Plugins::BotInfo,
+      Plugins::ByeDaddy,
       Plugins::Decide,
       Plugins::DelayedRejoin,
       Plugins::Dice,
+      Plugins::DJInfo,
       Plugins::JargonFile,
       Plugins::JoinNotice,
       Plugins::Kickban,
+      Plugins::Macros,
       Plugins::MultiQDB,
       Plugins::Ping,
       Plugins::PrivToolbox,
@@ -84,6 +85,7 @@ bot = Cinch::Bot.new do
       Plugins::RussianRoulette,
       Plugins::Ryder,
       Plugins::Silly,
+      Plugins::TimeBan,
       Plugins::Toolbox,
       Plugins::Twitter::Client,
       Plugins::UrbanDictionary,
@@ -96,15 +98,20 @@ bot = Cinch::Bot.new do
 
     c.plugins.options[Plugins::Authtest] = c.plugins.options[Plugins::AdminHandler] = c.plugins.options[Plugins::Toolbox] = c.plugins.options[Plugins::PrivToolbox] = c.plugins.options[Plugins::RemoteAdmin] = c.plugins.options[Plugins::AntiSpam::Lister] = common_config
 
-    c.plugins.options[Plugins::AntiSpam::Kicker] = { limit_commands: 5, limit_seconds: 60 }
+    c.plugins.options[Plugins::AntiSpam::Kicker] = { limit_commands: 5, limit_seconds: 30 }
     c.plugins.options[Plugins::Attack] = { attack_dictionary: File.dirname(__FILE__) + '/config/attackdict.yaml' }
     c.plugins.options[Plugins::AutoOP] = { enabled_channels: [] }.merge common_config
     c.plugins.options[Plugins::AutoVoice] = { enabled_channels: [] }.merge common_config
-    c.plugins.options[Plugins::BotInfo] = { owner: "Azure", bot: c.nick, template: File.dirname(__FILE__) + '/config/info_template.txt'}.merge common_config
+    c.plugins.options[Plugins::BotInfo] = { owner: "Azure", bot: c.nick, template_path: File.dirname(__FILE__) + '/config/info_template.txt'}.merge common_config
+    c.plugins.options[Plugins::DJInfo] = { dj_info: File.dirname(__FILE__) + '/config/cr_dj_info.yaml' }
     c.plugins.options[Plugins::JoinNotice] = { greetings: File.dirname(__FILE__) + '/config/greetings/freenode/', filext: '.txt' }
+    c.plugins.options[Plugins::Macros] = { macro_yaml_path: File.dirname(__FILE__) + '/config/macros.yaml' }
     c.plugins.options[Plugins::Twitter::Client] = { access_keys: File.dirname(__FILE__) + '/config/twitter_oauth.yaml' }
   end
 
 end
+
+bot.loggers << Cinch::Logger::ZcbotLogger.new(open('/home/azure/bots/logs/bot/freenode-zcbot.log','a'))
+bot.loggers << Cinch::Logger::FormattedLogger.new(open('/home/azure/bots/logs/bot/freenode-std'+Time.now.strftime("%Y-%m-%dT%H%.M.%SZ%z")+'.log','a'))
 
 bot.start
