38fc1dd679d8e710713a98dba3f68071fe6bc3e8
Mark Seymour <mark.seymour.ns@gmail.com>
2012-07-27 04:18:11 -0300

    Admin mixin update

--- lib/cinch/admin.rb
+++ lib/cinch/admin.rb
@@ -9,7 +9,7 @@ module Cinch
         user.authed?
     end
 
-    def is_trusted? (user)
+    def is@@trusted? (user)
       cc = Channel(shared[:controlchannel])
       cc.users.member?(user) &&
         cc.users[user].any? {|mode| @@types[:trusted].include? mode } &&
