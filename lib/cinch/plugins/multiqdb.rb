76ba8734f1a767f9a6acf553999ac82c4103aae0
Mark Seymour <mark.seymour.ns@gmail.com>
2012-10-05 14:21:07 -0300

    Rewritten QDB output formatting

--- lib/cinch/plugins/multiqdb.rb
+++ lib/cinch/plugins/multiqdb.rb
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-require 'zlib'
+#require 'zlib'
 
 require_relative 'qdb/bash'
 require_relative 'qdb/qdbus'
@@ -12,12 +12,14 @@ module Cinch
     class MultiQDB
       include Cinch::Plugin
 
+      QDB_LINE_MARKS = {normal: '-', contd: '+', end: '$'}
+
       set(
       plugin_name: "QDB",
       help: "Pulls a quote from a QDB.\n`Usage: !qdb <selector> <ID|latest|random>`; `!qdb` for selector list.",
       required_options: [:limit])
 
-      def initialize *args
+      def initialize(*args)
         super
         # Creating a hash of all QDB objects available.
         names = [QDB::Bash, QDB::QdbUS, QDB::Shakesoda, QDB::MIT].map {|qdb| [qdb.new.shortname, qdb] }
@@ -25,55 +27,63 @@ module Cinch
       end
 
       match /qdb(?: (\w+))(?: (.+))/, method: :execute_qdb, group: :x_qdb
-      def execute_qdb m, selector=nil, id=nil
+      def execute_qdb(m, selector=nil, id=nil)
         m.reply "You have not supplied a selector. Selectors: #{@@qdbs.keys * ", "}" and return if !selector
         m.reply "#{selector} does not exist. Valid selectors: #{@@qdbs.keys * ", "}" and return if !@@qdbs.include?(selector)
 
         qdb = @@qdbs[selector].new # Instantize the QDB object
 
-        result = case id
-        when "latest" then qdb.latest
-        when "random" then qdb.random
-        when /^#?[[:digit:]]+$/ then qdb.by_id(id.gsub(/\D+/, ''))
-        else
-          qdb.random
-        end
+        result = fetch_qdb_obj(id, qdb)
 
-        banner = "#{qdb.name} ##{result.id}"
         #rcolors = [:green, :red, :purple, :yellow, :lime, :teal, :aqua, :royal, :pink]
         #colorize_text = ->(text) { (Zlib::crc32(text) % rcolors.size)-1 }
-        m.reply banner
+        qdb_banner = "#{qdb.name} ##{result.id}"
+        m.reply qdb_banner
         result.quote.each_with_index {|q,i|
-          is_at_limit = -> {i+1 == config[:limit] && config[:limit] < result.quote.size}
-          if (1..config[:limit]).include?(i+1)
-            m.reply "#{is_at_limit.call ? "+" : "-"} #{q}"
-            m.reply "The full quote can be viewed online at #{qdb.url + qdb.id_path_template % {id: result.id}}." if is_at_limit.call
+          marker = if i.succ == result.quote.length then QDB_LINE_MARKS[:end]
+          elsif i.succ == config[:limit] then QDB_LINE_MARKS[:contd]
+          else QDB_LINE_MARKS[:normal]
+          end
+
+          if i.succ == config[:limit]
+            # qdb public/private boundary
+            m.reply "%s %s\nThe full quote can be viewed online at %s" % [marker, q, qdb.url + qdb.id_path_template % {id: result.id}]
+            m.user.notice "%s continued:" % qdb_banner
+          elsif i.succ > config[:limit]
+            # qdb private
+            m.user.notice "%s %s" % [marker, q]
           else
-            m.user.notice "#{banner} continued:" if i+1 == config[:limit]+1
-            bar = -> {i+1 == config[:limit]+1}
-            m.user.notice "- " + q
+            # qdb public
+            m.reply "%s %s" % [marker, q]
           end
         }
       rescue QDB::Error::QuoteNotFound => e
         m.reply e.message, true
       rescue OpenURI::HTTPError => e
         m.reply "An error has occured. #{$!}", true
-      rescue Errno::ETIMEDOUT
-        if retries > 0
-          retries = retries.pred
-          retry
-        else m.reply "I cannot access the selected QDB (#{selector}, ##{id}) at the moment. Please try again later.", true
-        end
       end
 
       match "qdb list", method: :execute_list
-      def execute_list m
+      def execute_list(m)
         list = []
         @@qdbs.each_value {|q|
           qdb = q.new
           list << "#{q.name} (#{q.shortname}): #{q.url}"
         }
         m.notice "To use a QDB, type " + Format(:bold, "!qdb <selector> <ID|latest|random>") + ".\n" + list.join("\n")
+      end
+
+      private
+
+      # @param id [Integer,String] The ID of the qdb entry. It may be an integer, a stringed integer with an octothorpe at the beginning (`#30`), `latest`, or `random`.
+      # @param qdb [QDB::Base] The QDB object.
+      def fetch_qdb_obj(id, qdb)
+        case id
+        when "latest" then qdb.latest
+        when "random" then qdb.random
+        when /^#?[[:digit:]]+$/ then qdb.by_id(id.gsub(/\D+/, ''))
+        else qdb.random
+        end
       end
 
     end
