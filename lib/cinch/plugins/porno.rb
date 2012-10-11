eaafce9294f47c953c89f827e90c0a177a233b8c
Mark Seymour <mark.seymour.ns@gmail.com>
2012-10-11 02:59:49 -0300

    Got rid of legacy code that made its way into the new codebase

--- lib/cinch/plugins/porno.rb
+++ lib/cinch/plugins/porno.rb
@@ -1,5 +1,3 @@
-require 'json'
-require 'open-uri'
 require 'yaml'
 
 module Cinch
@@ -7,22 +5,12 @@ module Cinch
     class Porno
       include Cinch::Plugin
 
+      set required_options: [:porno_list_path]
+
       def initialize(*args)
         super
-        # Gets listing, adds yaml file to listing, gets unique values, and saves.
-        @pornos = get_listing
-        @pornos << YAML.load(open(config[:porno_list_path])) if File.exist?(config[:porno_list_path])
-        @pornos.uniq!
-        open(config[:porno_list_path], 'w+') {|file| file.write(@pornos.to_yaml)}
-
-        # does the same, but on a timer.
-        @timer = Timer(7200) { 
-          updates = get_listing
-          updates ||= @pornos
-          @pornos << updates
-          @pornos.uniq! 
-          open(config[:porno_list_path], 'w+') {|file| file.write(@pornos.to_yaml)}
-        }
+        update_porno_list
+        @timer = Timer(7200, method: :update_porno_list)
       end
 
       match /porno(?> )?(.+)?/, method: :execute_porno
@@ -37,17 +25,8 @@ module Cinch
 
       private
 
-      def get_listing
-        pornos = JSON.parse(open(%q{http://www.directv.com/entertainment/data/guideScheduleSegment.json.jsp?numchannels=14&channelnum=586&blockdur=24}).read)
-
-        return nil if !pornos["success"]
-
-        pornos["channels"].each_with_object([]) {|channel,memo|
-          channel["schedules"].each {|program|
-            next if program["productType"].empty? || !program["repeat"]
-            memo << program["prTitle"]
-          }
-        }.uniq
+      def update_porno_list
+        @pornos = YAML.load(open(config[:porno_list_path]))
       end
     end
   end
