676b3d6ceee0b33097d2d4584c4e26ae50413525
Mark Seymour <mark.seymour.ns@gmail.com>
2013-03-02 02:27:17 -0400

    Only load the YAML file when requested rather than storing it in RAM.

--- lib/cinch/plugins/porno.rb
+++ lib/cinch/plugins/porno.rb
@@ -7,26 +7,15 @@ module Cinch
 
       set help: "Random porno film names.\nUsage: `!porno [search term]`", required_options: [:porno_list_path]
 
-      def initialize(*args)
-        super
-        update_porno_list
-        @timer = Timer(7200, method: :update_porno_list)
-      end
-
       match /porno(?> )?(.+)?/, method: :execute_porno
       def execute_porno(m, search)
+        pornos = YAML.load(open(config[:porno_list_path]))
         if search
-          results = @pornos.find_all {|title| title =~ /#{Regexp.escape(search)}/i }
-          m.reply (results.empty? ? @pornos : results).sample, true
+          results = pornos.find_all {|title| title =~ /#{Regexp.escape(search)}/i }
+          m.reply (results.empty? ? pornos : results).sample, true
         else
-          m.reply @pornos.sample, true
+          m.reply pornos.sample, true
         end
-      end
-
-      private
-
-      def update_porno_list
-        @pornos = YAML.load(open(config[:porno_list_path]))
       end
     end
   end
