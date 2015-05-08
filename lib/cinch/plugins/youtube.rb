2b4fa37e46b205e17a0e1a4433ca3260075bb8f8
Mark Seymour <mark.seymour.ns@gmail.com>
2015-05-08 20:33:04 -0300

    Upgrade to YouTube API v3
    Now the plugin requires an API key. When configuring your bot with this plugin, be sure to pass in a configuration key of `:api_key` with your public API key as the value.
    Please read https://developers.google.com/youtube/v3/getting-started for more info.

--- lib/cinch/plugins/youtube.rb
+++ lib/cinch/plugins/youtube.rb
@@ -2,35 +2,38 @@
 
 require_relative '../helpers/is_channel_disabled'
 require 'httparty'
+require 'iso8601'
 
 module Cinch
   module Plugins
     class YouTube
       include Cinch::Plugin
+      set(
+        required_options: [:api_key])
 
       # From Google's Closure Library, with slight modifications:
       # http://closure-library.googlecode.com/svn/docs/closure_goog_ui_media_youtube.js.source.html
-      YOUTUBE_VIDEO_REGEXP = /https?:\/\/(?:[a-zA-Z]{2,3}\.)?(?:youtube\.com\/watch)(?:\?(?:[\w=-]+&(?:amp;)?)*v=([\w-]+)(?:&(?:amp;)?[\w=-]+)*)?(?:#[!]?(?:(?:(?:[\w=-]+&(?:amp;)?)*(?:v=([\w-]+))(?:&(?:amp;)?[\w=-]+)*)|(?:[\w=&-]+)))?[^\w-]?|http:\/\/(?:[a-zA-Z]{2,3}\.)?(?:youtu\.be\/)([\w-]+)/i
-      YOUTUBE_API_VIDEO_URL = "http://gdata.youtube.com/feeds/api/videos/%s?v=2&alt=jsonc"
+      YOUTUBE_VIDEO_REGEXP = /https?:\/\/(?:[a-zA-Z]{2,3}\.)?(?:youtube\.com\/watch)(?:\?(?:[\w=-]+&(?:amp;)?)*v=([\w-]+)(?:&(?:amp;)?[\w=-]+)*)?(?:#[!]?(?:(?:(?:[\w=-]+&(?:amp;)?)*(?:v=([\w-]+))(?:&(?:amp;)?[\w=-]+)*)|(?:[\w=&-]+)))?[^\w-]?|https?:\/\/(?:[a-zA-Z]{2,3}\.)?(?:youtu\.be\/)([\w-]+)/i
+      YOUTUBE_API_VIDEO_URL = "https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics,contentDetails&id=%s&key=%s"
 
       listen_to :channel, method: :listen_to_channel
       def listen_to_channel(m)
         return if is_channel_disabled?(m.channel)
         return unless m.message.match(YOUTUBE_VIDEO_REGEXP)
-        videos = m.message.scan(YOUTUBE_VIDEO_REGEXP).flatten.reject(&:'nil?').uniq
+        video_ids = m.message.scan(YOUTUBE_VIDEO_REGEXP).flatten.reject(&:'nil?').uniq
+        response = HTTParty.get(YOUTUBE_API_VIDEO_URL % [video_ids.join(','), config[:api_key]], headers: {'User-Agent' => "HTTParty/#{HTTParty::VERSION} #{RUBY_ENGINE}/#{RUBY_VERSION}"})
+        raise StandardError, '%s - %s (%d)' % [response['error']['message'], v, response['error']['code']] if response['error']
+        videos = response['items']
 
-        videos.each {|v|
-          response = HTTParty.get(YOUTUBE_API_VIDEO_URL % v, headers: {'User-Agent' => "HTTParty/#{HTTParty::VERSION} #{RUBY_ENGINE}/#{RUBY_VERSION}"})
-          raise StandardError, '%s - %s (%d)' % [response['error']['message'], v, response['error']['code']] if response['error']
-          video = response['data']
+        videos.each {|video|
           m.reply "#{Format(:bold,'YouTube »')} #{Format(:purple,'%<title>s')} (%<length>s) · by %<uploader>s on %<uploaded>s · #{Format(:green,'☝%<likes>s')} #{Format(:red,'☟%<dislikes>s')} · %<views>s views" % {
-            title: video['title'],
-            uploader: video['uploader'],
-            uploaded: Time.parse(video['uploaded']).strftime('%F'),
-            length: seconds_to_time(video['duration']),
-            likes: commify_numbers(video['likeCount'].to_i),
-            dislikes: commify_numbers(video['ratingCount'].to_i - video['likeCount'].to_i),
-            views: commify_numbers(video['viewCount'].to_i)
+            title: video['snippet']['title'],
+            uploader: video['snippet']['channelTitle'],
+            uploaded: Time.parse(video['snippet']['publishedAt']).strftime('%F'),
+            length: seconds_to_time(ISO8601::Duration.new(video['contentDetails']['duration']).to_seconds),
+            likes: commify_numbers(video['statistics']['likeCount'].to_i),
+            dislikes: commify_numbers(video['statistics']['dislikeCount'].to_i),
+            views: commify_numbers(video['statistics']['viewCount'].to_i)
           }
         }
       rescue => e
